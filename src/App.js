ec2a3d1afb069e96d8597cf8a5bc03e42e1fd404
Riazul Islam <ri64627@gmail.com>
2020-05-05 11:07:30 +0600

    added custom login data with gmail signin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleLoginPopup = () => {
+    firebase.auth().signInWithPopup(provider).then(result =>{
+      const {displayName, email, photoURL} = result.user;
+      console.log(displayName, email);
+    });
+  };
+  // firebase.auth().signInWithPopup(provider).then( result =>{
+  //   console.log(result);
+  // });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={handleLoginPopup}>Sign In</button>
     </div>
   );
 }
